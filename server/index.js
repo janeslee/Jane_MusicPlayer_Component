3f8abb94cdb46ec90482c234c2dde034051af478
Jane Lee <sohyunlee53@gmail.com>
2018-12-19 18:25:35 -0800

    Set up basic server. Verified connection to port 3000

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.listen(3000, () => { console.log('listening to port 3000') });
