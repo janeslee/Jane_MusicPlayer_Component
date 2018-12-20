bc1936d5e1175a132f4fa1839121bc432499a73e
Jane Lee <sohyunlee53@gmail.com>
2018-12-19 20:14:19 -0800

    Created database. Verified database connection with example POST request in Postman

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('../database/index');
 
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const server = express();
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/../client/dist'));
+server.use(express.static(__dirname + '/../client/dist'));
 
-app.listen(3000, () => { console.log('listening to port 3000') });
+server.listen(3000, () => { console.log('listening to port 3000') });
+
+server.post('/api/jane/player/', (req, res) => {
+  db.songs.create({ 
+    title: req.body.title
+  })
+  .then(() => { console.log('post success') })
+  .catch(error => { console.error(error) });
+});
+
+module.exports = server;
