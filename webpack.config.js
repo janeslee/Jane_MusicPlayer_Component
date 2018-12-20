4370621a42ede10bd4873ba6674b2a6062ecde19
Jane Lee <sohyunlee53@gmail.com>
2018-12-19 18:14:21 -0800

    Initializing repo with wepback and package.json

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'env']
+        },
+      },
+    ],
+  },
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js',
+  }
+};
