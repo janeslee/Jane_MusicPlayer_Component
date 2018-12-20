b7ffa9fe543cf9823e6d80d8e034d1aac3b68ea3
Jane Lee <sohyunlee53@gmail.com>
2018-12-19 21:55:42 -0800

    Verified Equalizer component rendering via Express server

---
+++ client/src/components/equalizer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Equalizer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Rendering from EQUALIZER</h1>
+      </div>
+    )
+  }
+}
+
+export default Equalizer;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Equalizer from './components/equalizer.jsx';
 
+ReactDOM.render(<Equalizer />, document.getElementById('root'));
