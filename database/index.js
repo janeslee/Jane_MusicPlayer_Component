47f89f9186ab3ab74b663cbcb2ef0843ff79c338
Jane Lee <sohyunlee53@gmail.com>
2018-12-21 11:42:16 -0800

    Modified schema to add wave data. Generated fake image data and verified in database. Fixed var declaration redundancy

--- database/index.js
+++ database/index.js
@@ -18,8 +18,9 @@ const songs = db.define(
     album: { type: Sequelize.STRING },
     released: { type: Sequelize.DATE },
     duration: { type: Sequelize.INTEGER },
+    wave: { type: Sequelize.TEXT },
+    image: { type: Sequelize.STRING },
     song_url: { type: Sequelize.STRING },
-    image_url: { type: Sequelize.STRING }
   },
   {
     timestamps: false 
--- database/seed.js
+++ database/seed.js
@@ -1,7 +1,7 @@
 const faker = require('faker');
 const db = require('./index');
 
-//have one song to be played for the first page
+//have one song to be played for every song ID but every song ID will have different wave form
 
 
 
@@ -11,24 +11,30 @@ var randomArtists = [];
 var randomAlbums = [];
 var randomReleased = [];
 var randomDuration = [];
+var randomImage = [];
 
 //function to randomize data for each column in db
 dataGenerator = (type, array) => {
+  var randomData;
+
   for (var i = 0; i <= 100; i++) {
-    if (type === "titles" || type === "albums") {
-      var randomData = faker.lorem.word();
+    if (type === 'titles' || type === 'albums') {
+      randomData = faker.lorem.word();
     }
-    if (type === "artists") {
-      var randomData = faker.name.findName();
+    if (type === 'artists') {
+      randomData = faker.name.findName();
     }
-    if (type === "released") {
-      var randomData = faker.date.past();
+    if (type === 'released') {
+      randomData = faker.date.past();
     }
-    if (type === "duration") {
-      var randomData = faker.random.number({
+    if (type === 'duration') {
+      randomData = faker.random.number({
         'min': 100,
         'max': 400
       });
+    }
+    if (type === 'image') {
+      randomData = faker.image.abstract();
     }
     array.push(randomData);
   }
@@ -39,6 +45,11 @@ dataGenerator('albums', randomAlbums);
 dataGenerator('artists', randomArtists);
 dataGenerator('released', randomReleased);
 dataGenerator('duration', randomDuration);
+dataGenerator('image', randomImage);
+
+//create a function to generate random wave data
+
+
 
 //create an entry for each item in the db
 for (var i = 0; i <= 100; i++) {    
@@ -47,6 +58,8 @@ for (var i = 0; i <= 100; i++) {
     artist: randomArtists[i],
     album: randomAlbums[i],
     released: randomReleased[i],
-    duration: randomDuration[i]
+    duration: randomDuration[i],
+    image: randomImage[i]
+    //song_url: should point to the same url for every data
   });
 }
