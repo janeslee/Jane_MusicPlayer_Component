308f5e91b37caa7d15a860be89c703db62e542e2
Jane Lee <sohyunlee53@gmail.com>
2018-12-21 15:35:23 -0800

    Fixed styling according to airbnb styleguide

--- database/seed.js
+++ database/seed.js
@@ -1,27 +1,27 @@
 const faker = require('faker');
 const db = require('./index');
 
-//alternate between 5 songs to be played for each data, but each data will have diff. wave form
-var randomSongs = [
+// alternate between 5 songs to be played for each data, but each data will have diff. wave form
+const randomSongs = [
   'https://s3-us-west-1.amazonaws.com/democrituscloud/Despacito.mp3',
   'https://s3-us-west-1.amazonaws.com/democrituscloud/Pokemon_Theme_Song.mp3',
   'https://s3-us-west-1.amazonaws.com/democrituscloud/RASPUTIN_-_Funk_Overload.mp3',
   'https://s3-us-west-1.amazonaws.com/democrituscloud/bensound-dubstep.mp3',
-  'https://s3-us-west-1.amazonaws.com/democrituscloud/bensound-jazzyfrenchy.mp3'
+  'https://s3-us-west-1.amazonaws.com/democrituscloud/bensound-jazzyfrenchy.mp3',
 ];
 
-var randomTitles = [];
-var randomArtists = [];
-var randomAlbums = [];
-var randomReleased = [];
-var randomDuration = [];
-var randomImage = [];
+const randomTitles = [];
+const randomArtists = [];
+const randomAlbums = [];
+const randomReleased = [];
+const randomDuration = [];
+const randomImage = [];
 
-//function to randomize data for each column in db
-dataGenerator = (type, array) => {
-  var randomData;
+// function to randomize data for each column in db
+const dataGenerator = (type, array) => {
+  let randomData;
 
-  for (var i = 0; i <= 100; i++) {
+  for (let i = 0; i <= 100; i += 1) {
     if (type === 'titles' || type === 'albums') {
       randomData = faker.lorem.word();
     }
@@ -33,8 +33,8 @@ dataGenerator = (type, array) => {
     }
     if (type === 'duration') {
       randomData = faker.random.number({
-        'min': 100,
-        'max': 300
+        min: 100,
+        max: 300,
       });
     }
     if (type === 'image') {
@@ -51,18 +51,18 @@ dataGenerator('released', randomReleased);
 dataGenerator('duration', randomDuration);
 dataGenerator('image', randomImage);
 
-//create a function to generate random wave data in string form, later to be split into an array
-waveGenerator = () => {
-  var waves = '';
-  for (var i = 0; i <= 1000; i++) {
-    var wave = Math.floor((Math.random() * 120));
+// create a function to generate random wave data in string form, later to be split into an array
+const waveGenerator = () => {
+  let waves = '';
+  for (let i = 0; i <= 1000; i += 1) {
+    const wave = Math.floor((Math.random() * 120));
     waves += `${wave},`;
   }
   return waves;
 };
 
-//create an entry for each item in the db
-for (var i = 0; i <= 100; i++) {    
+// create an entry for each item in the db
+for (let i = 0; i <= 100; i += 1) {
   db.songs.create({
     title: randomTitles[i],
     artist: randomArtists[i],
@@ -71,6 +71,6 @@ for (var i = 0; i <= 100; i++) {
     duration: randomDuration[i],
     wave: waveGenerator(),
     image: randomImage[i],
-    song_url: randomSongs[Math.floor(Math.random() * 5)]
+    song_url: randomSongs[Math.floor(Math.random() * 5)],
   });
 }
--- server/index.js
+++ server/index.js
@@ -1,29 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const db = require('../database/index');
 
 const server = express();
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
-server.use(express.static(__dirname + '/../client/dist'));
+server.use(express.static(path.join(__dirname, '/../client/dist')));
 
-server.listen(3000, () => { console.log('listening to port 3000') });
+server.listen(3000, () => { console.log('listening to port 3000'); });
 
-//example POST request to verify database connection
-server.post('/api/jane/player/', (req, res) => {
-  db.songs.create({ 
-    title: req.body.title
-  })
-  .then(() => { console.log('post success') })
-  .catch(error => { console.error(error) });
-});
-
-//GET request to fetch a new song data from db
+// GET request to fetch a new song data from db
 server.get('/api/jane/player/', (req, res) => {
   db.songs.findById(req.body.id)
-  .then(data => { res.send(data).status(200) })
-  .catch(error => { console.error(error) });
+    .then((data) => { res.send(data).status(200); })
+    .catch((error) => { console.error(error); });
 });
 
 module.exports = server;
