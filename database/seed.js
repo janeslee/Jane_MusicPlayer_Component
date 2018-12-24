5fe375aa2cb200a3e952cbb125dfe274a4565e45
Jane Lee <sohyunlee53@gmail.com>
2018-12-24 12:15:56 -0800

    Simplified the seeding script by eliminating if/else statements according to Robin's suggestion

--- database/seed.js
+++ database/seed.js
@@ -10,47 +10,6 @@ const randomSongs = [
   'https://s3-us-west-1.amazonaws.com/democrituscloud/bensound-jazzyfrenchy.mp3',
 ];
 
-const randomTitles = [];
-const randomArtists = [];
-const randomAlbums = [];
-const randomReleased = [];
-const randomDuration = [];
-const randomImage = [];
-
-// function to randomize data for each column in db
-const dataGenerator = (type, array) => {
-  let randomData;
-
-  for (let i = 0; i <= 100; i += 1) {
-    if (type === 'titles' || type === 'albums') {
-      randomData = faker.lorem.word();
-    }
-    if (type === 'artists') {
-      randomData = faker.name.findName();
-    }
-    if (type === 'released') {
-      randomData = faker.date.past();
-    }
-    if (type === 'duration') {
-      randomData = faker.random.number({
-        min: 100,
-        max: 300,
-      });
-    }
-    if (type === 'image') {
-      randomData = faker.image.abstract();
-    }
-    array.push(randomData);
-  }
-};
-
-dataGenerator('titles', randomTitles);
-dataGenerator('albums', randomAlbums);
-dataGenerator('artists', randomArtists);
-dataGenerator('released', randomReleased);
-dataGenerator('duration', randomDuration);
-dataGenerator('image', randomImage);
-
 // create a function to generate random wave data in string form, later to be split into an array
 const waveGenerator = () => {
   let waves = '';
@@ -64,13 +23,13 @@ const waveGenerator = () => {
 // create an entry for each item in the db
 for (let i = 0; i <= 100; i += 1) {
   db.songs.create({
-    title: randomTitles[i],
-    artist: randomArtists[i],
-    album: randomAlbums[i],
-    released: randomReleased[i],
-    duration: randomDuration[i],
+    title: faker.lorem.word(),
+    artist: faker.name.findName(),
+    album: faker.lorem.word(),
+    released: faker.date.past(),
+    duration: faker.random.number({ min: 100, max: 300 }),
     wave: waveGenerator(),
-    image: randomImage[i],
+    image: faker.image.abstract(),
     song_url: randomSongs[Math.floor(Math.random() * 5)],
   });
 }
