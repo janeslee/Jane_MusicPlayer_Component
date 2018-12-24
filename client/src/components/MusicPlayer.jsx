c61b8d531a84a30497c74a29fb39e236c02aadbe
Jane Lee <sohyunlee53@gmail.com>
2018-12-24 14:09:30 -0800

    Rendering play and pause buttons. Toggling function implemented

--- client/src/components/MusicPlayer.jsx
+++ client/src/components/MusicPlayer.jsx
@@ -3,21 +3,47 @@ import CSSModules from 'react-css-modules';
 import styles from '../styles/MusicPlayer.css';
 // import axios from 'axios';
 
+const imagePaths = {
+  pauseButton: 'https://s3-us-west-1.amazonaws.com/democrituscloud/pause.png',
+  playButton: 'https://s3-us-west-1.amazonaws.com/democrituscloud/play.png'
+}
+
 class MusicPlayer extends React.Component {
   constructor(props) {
     super(props);
+    
     this.state = {
-      playButton: 'https://s3-us-west-1.amazonaws.com/democrituscloud/pause.png'
+      play: false
+    }
+    this.clickHandler = this.clickHandler.bind(this);
+    this.playStatus = this.playStatus.bind(this);
+  }
+
+  clickHandler(event) {
+    this.setState({
+      play: !this.state.play
+    });
+    console.log(this.state.play);
+  }
+
+  playStatus() {
+    if (this.state.play === true) {
+      //play music
+    } else {
+      //pause music
     }
   }
 
   render() {
     return(
       <div>
-        <h1>Rendering from Music Player</h1>
-        <button styleName='playButton'>
-          <img src={this.state.playButton} />
-        </button>
+        {
+          this.state.play
+          ?
+          <img className={styles.Button} src={imagePaths.pauseButton} onClick={this.clickHandler} />
+          :
+          <img className={styles.Button} src={imagePaths.playButton} onClick={this.clickHandler} />
+        }
       </div>
     )
   }
