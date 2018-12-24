401a773f2ddf4cfd906da88197c17b7fedc649fd
Jane Lee <sohyunlee53@gmail.com>
2018-12-24 12:58:01 -0800

    Modified GET request to handle id via URL not req.body

--- server/index.js
+++ server/index.js
@@ -12,8 +12,10 @@ server.use(express.static(path.join(__dirname, '/../client/dist')));
 server.listen(3000, () => { console.log('listening to port 3000'); });
 
 // GET request to fetch a new song data from db
-server.get('/api/jane/player/', (req, res) => {
-  db.songs.findById(req.body.id)
+// let id;
+server.get('/api/jane/player/:id', (req, res) => {
+  const { id } = req.params;
+  db.songs.findByPk(id)
     .then((data) => { res.send(data).status(200); })
     .catch((error) => { res.send(error).status(500); });
 });
