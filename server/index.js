45b14a637c9c5721f39be8dcf90efa98716431ff
Jane Lee <sohyunlee53@gmail.com>
2018-12-24 18:27:03 -0800

    Need to look into rendering the same static content for the api endpoint that directs to individual song page

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ server.listen(3000, () => { console.log('listening to port 3000'); });
 // let id;
 server.get('/api/jane/player/:id', (req, res) => {
   const { id } = req.params;
+  // res.sendFile(path.join(__dirname, '/../client/dist/index.html'));
   db.songs.findByPk(id)
     .then((data) => { res.send(data).status(200); })
     .catch((error) => { res.send(error).status(500); });
