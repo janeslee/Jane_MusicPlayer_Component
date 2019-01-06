15b0ed2b4553fc544fc39b16c924b21efbb10133
Jane Lee <sohyunlee53@gmail.com>
2019-01-06 12:29:47 -0800

    Modified MusicPlayer wave state. Cleaned up some server code

--- server/index.js
+++ server/index.js
@@ -11,13 +11,11 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use('/', express.static('./client/dist/'));
 server.use(/\/\d+\//, express.static('./client/dist/'));
 
-server.listen(5000, () => { console.log('listening to port 5000'); });
+server.listen(8000, () => { console.log('listening to port 8000'); });
 
 // GET request to fetch a new song data from db
-// let id;
 server.get('/api/jane/player/:id', (req, res) => {
   const { id } = req.params;
-  // res.sendFile(path.join(__dirname, '/../client/dist/index.html'));
   db.songs.findByPk(id)
     .then((data) => { res.send(data).status(200); })
     .catch((error) => { res.send(error).status(500); });
